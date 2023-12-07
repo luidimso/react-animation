cbefff8c6ee01350bd41782a9faf48655f8003ca
luidimso <luidi_mso@outlook.com>
2023-12-07 04:39:22 -0300

    Using Framer-Motion modal entry animation

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,13 +1,22 @@
+import { motion } from 'framer-motion';
 import { createPortal } from 'react-dom';
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog open className="modal"
+      initial={{
+        opacity: 0,
+        y: 30
+      }}
+      animate={{
+        opacity: 1,
+        y: 0
+      }}>
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById('modal')
   );
